55355973fb265113ef19724f12540471a5810b53
Alexander <alexander.0894-rp@hotmail.com>
2021-11-09 21:13:44 +0000

    update class

--- models/base_model.py
+++ models/base_model.py
@@ -6,16 +6,19 @@ import uuid
 
 
 class BaseModel:
-    """"""
+    """BaseModel class"""
     id = str(uuid.uuid4())
     created_at = datetime.now()
     updated_at = datetime.now()
 
     def save(self):
+        """update instance with the current datetime"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """returns a dictionary containing all keys/values of dict"""
         pass
 
     def __str__(self):
+        """string representacion of object"""
         return ("[{}] ({:d}) {}").format(__class__.__name__, self.id, self.__dict__)
