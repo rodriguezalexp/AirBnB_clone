8457cad10e76f57e7580d3660232803f057623a5
Yaeluribe <2962@holbertonschool.com>
2021-11-13 14:49:03 -0500

    BaseModel unittest

---
+++ tests/test_base_model.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" Test_BaseModel: unittest for base_model"""
+import unittest
+from models.base_model import BaseModel
+
+
+class Test_BaseModel(unittest.TestCase):
+
+	def setUp(self):
+		self.object = BaseModel()
+
+	def testDocstring_class(self):
+		"""Check docstring in class"""
+		self.assertIsNotNone(BaseModel.__doc__)
+
+	def test_id(self):
+		"""Check if id is a string"""
+		self.assertEqual(type(self.object.id), str)
+
+	def testinstance(self):
+		"""Test if the instance belongs to BaseModel class"""
+		self.assertIsInstance(self.object, BaseModel)
+
+	def test_addattr(self):
+		"""Test adding a new attribute"""
+		self.object.name = "Edgar"
+		self.object.age = 66
+		self.assertTrue(isinstance(self.object, BaseModel))
+
+	def test_to_dict(self):
+		"""Tests the retrieved object is a dictionary"""
+		nu_dict = self.object.to_dict()
+		self.assertTrue(isinstance(nu_dict, dict))
+
+	def test_save(self):
+		""""""
