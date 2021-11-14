2ec5158284349713cb5a1b56772266180eb0f147
Yaeluribe <2962@holbertonschool.com>
2021-11-13 20:39:29 -0500

    using unnitest.mock library to simulate an object

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python3
 """ Test_BaseModel: unittest for base_model"""
 import unittest
-from console import HBNBCommand
+from unittest import mock  # we use this library so simulate an object...
+                            # a running console
 
 
 class TestConsole(unittest.TestCase):
     """Unittests for console.py"""
+
+    def test_quit(self):
+        """Test quit expression"""
+        self.mocked(cmd="quit", expected="")
+
     def test_EOF(self):
-        pass
+        """Test EOF expression, ctrl+D"""
+        self.mocked(cmd="EOF", expected="")
+
+    def test_emptyline(self):
+        """"Tests empty line.. do nothing"""
+        self.mocked(cmd="\n", expected="")
+    
+    
