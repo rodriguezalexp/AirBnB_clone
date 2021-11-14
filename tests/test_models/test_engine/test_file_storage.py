04151391081a8104cee4a7cfaca47dd69e74e67c
Yaeluribe <2962@holbertonschool.com>
2021-11-13 21:00:49 -0500

    Creating tests

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Module: Unittest for file_storage.py
+"""
+from _typeshed import Self
+import unittest
+from models.engine.file_storage import FileStorage
+
+class TestFilestorage(unittest.TestCase):
+	"""Class to run unittest on FileStorage"""
+
+	def setUp(self):
+		self.object = FileStorage()
+
+	def testDocstring_class(self):
+		"""Check class, docstring"""
+		self.assertIsNotNone(FileStorage.__doc__)
+
+	def testinstance(def):
+		"""test if instance of object belongs to FileStorage"""
+		self.assertIsInstance(self.object, FileStorage)
+	
+	def test_all():
+		"""Tests all() method"""
+		self.assertIs
