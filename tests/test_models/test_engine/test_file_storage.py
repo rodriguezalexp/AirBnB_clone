12abc619145c7c389d7410b1c8e346b1b0fe00f6
Yaeluribe <2962@holbertonschool.com>
2021-11-13 21:09:33 -0500

    pep8 compliant

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -6,20 +6,21 @@ from _typeshed import Self
 import unittest
 from models.engine.file_storage import FileStorage
 
+
 class TestFilestorage(unittest.TestCase):
-	"""Class to run unittest on FileStorage"""
+    """Class to run unittest on FileStorage"""
 
-	def setUp(self):
-		self.object = FileStorage()
+    def setUp(self):
+        self.object = FileStorage()
 
-	def testDocstring_class(self):
-		"""Check class, docstring"""
-		self.assertIsNotNone(FileStorage.__doc__)
+    def testDocstring_class(self):
+        """Check class, docstring"""
+        self.assertIsNotNone(FileStorage.__doc__)
 
-	def testinstance(def):
-		"""test if instance of object belongs to FileStorage"""
-		self.assertIsInstance(self.object, FileStorage)
-	
-	def test_all():
-		"""Tests all() method"""
-		self.assertIs
+    def testinstance(def):
+        """test if instance of object belongs to FileStorage"""
+        self.assertIsInstance(self.object, FileStorage)
+
+    def test_all():
+        """Tests all() method"""
+        self.assertIs
