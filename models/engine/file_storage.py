2376abbc07562a48987440654f253bd4bfbfcfd8
Yaeluribe <2962@holbertonschool.com>
2021-11-14 12:03:37 -0500

    pffff

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,12 +3,6 @@
 Module: FileStorage
 """
 from models.base_model import BaseModel
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
 import json
 
 
