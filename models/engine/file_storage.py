82c277813fbcd616f662ee0d55ef4bc4d1fe9dad
Alexander <alexander.0894-rp@hotmail.com>
2021-11-15 00:19:37 +0000

    update filestorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,6 +3,12 @@
 Module: FileStorage
 """
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.review import Review
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
 import json
 
 
