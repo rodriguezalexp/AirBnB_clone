1f0337b20430acbf30981335bbed70b396ebb8ff
Yaeluribe <2962@holbertonschool.com>
2021-11-15 12:17:06 -0500

    amenity unittest

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -5,4 +5,18 @@ from models.amenity import Amenity
 
 
 class TestAmenity(unittest.TestCase):
-	"""unittest for class Amenity"""
+    """unittest for class Amenity"""
+    def setUp(self):
+        self.object = Amenity
+
+    def testdocstring(self):
+        """class check"""
+        self.assertIsNotNone(Amenity.__doc__)
+
+    def testinstance(self):
+        """Test if instance belongs to Amenity"""
+        self.assertIsInstance(self.object, Amenity)
+
+
+if __name__ == '__main__':
+    unittest.main()
